d24146d82aee9b2b6b6ab69022d54b875963b167
ParkJeomuk <parkjeomuk@gmail.com>
2022-01-12 16:02:17 +0900

    commit

--- routes.py
+++ routes.py
@@ -6,6 +6,11 @@ from pages.tab_cards.callbacks   import display_tabbox1
 from pages.gallery_1.callbacks import make_graph
 from pages.gallery_2.callbacks import update_figure
 
+# Dash Page --------------------------------------------------------------------------------
+from pages.dash_pages.callbacks import dash_data_load
+from pages.dash_pages.callbacks import dash_plot1_render_click
+from pages.dash_pages.callbacks import dash_plot2_render
+
 # @app.callback(
 #     Output("page-content", "children"),
 #     [Input("url", "pathname")] )  
