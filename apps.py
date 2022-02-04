94cef21ed20780bd0d9684447ab167aefe885119
ParkJeomuk <parkjeomuk@gmail.com>
2022-02-04 14:33:45 +0900

    commit

--- apps.py
+++ apps.py
@@ -1,15 +1,31 @@
 import dash
 import dash_bootstrap_components as dbc
+# import uvicorn as uvicorn
+# from fastapi import FastAPI
 
 from utils.external_assets import ROOT, EXTERNAL_STYLESHEETS, FONT_AWSOME
+
+#-------- Flask --------------------------------------------
 import flask
 # from flask_caching import Cache
+
+#-------- FastAPI ------------------------------------------
+# from fastapi import FastAPI
+# from starlette.middleware.wsgi import WSGIMiddleware
+
+
 
 from ui.main import layout
 # =============================================================================
 # Dash App and Flask Server
 # =============================================================================
+
+
+
 server = flask.Flask(__name__)
+
+# server = FastAPI()
+
 
 app = dash.Dash(
     name= __name__,
@@ -17,7 +33,7 @@ app = dash.Dash(
     routes_pathname_prefix='/dash/',
     requests_pathname_prefix="/dash/",
     assets_folder = ROOT+"/assets/", 
-
+    title = "BECOM",
     suppress_callback_exceptions=True, 
     external_stylesheets=[
         dbc.themes.BOOTSTRAP  , 
@@ -28,6 +44,9 @@ app = dash.Dash(
         {"name": "viewport", "content": "width=device-width, initial-scale=1"}
     ]
 )
+
+# server.mount("/dash", WSGIMiddleware(app.server))
+# uvicorn.run(server)
 
 # cfg = {
 #     'DEBUG' : True,
